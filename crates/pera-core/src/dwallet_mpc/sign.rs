2b4286fa46a001c47e20d2e74757f33692b1eb84
Itay <itaylevy134@gmail.com>
2024-11-10 15:39:40 +0200

    keep workin

--- crates/pera-core/src/dwallet_mpc/sign.rs
+++ crates/pera-core/src/dwallet_mpc/sign.rs
@@ -1,9 +1,12 @@
+use crate::dwallet_mpc::dkg::{AsyncProtocol, DKGFirstParty, DKGSecondParty};
+use crate::dwallet_mpc::presign::{
+    FirstSignBytesParty, PresignSecondParty, SignAuxiliaryInput, SignFirstParty,
+};
 use group::PartyID;
 use mpc::{Output, Party};
+use twopc_mpc::class_groups::Presign;
 use twopc_mpc::dkg::Protocol;
 use twopc_mpc::paillier::Protocol;
-use crate::dwallet_mpc::dkg::{AsyncProtocol, DKGFirstParty, DKGSecondParty};
-use crate::dwallet_mpc::presign::{FirstSignBytesParty, PresignSecondParty, SignAuxiliaryInput, SignFirstParty};
 
 impl FirstSignBytesParty {
     pub(crate) fn generate_auxiliary_input(
@@ -15,12 +18,30 @@ impl FirstSignBytesParty {
         first_round_output: Vec<u8>,
     ) -> Vec<u8> {
         let first_round_output = bcs::from_bytes(&first_round_output).unwrap();
-        let auxiliary_auxiliary_input = crate::dwallet_mpc::dkg::DKGFirstParty::generate_auxiliary_input(
-            session_id.clone(),
-            number_of_parties,
-            party_id,
-        );
+        let auxiliary_auxiliary_input =
+            crate::dwallet_mpc::dkg::DKGFirstParty::generate_auxiliary_input(
+                session_id.clone(),
+                number_of_parties,
+                party_id,
+            );
 
-        (auxiliary_auxiliary_input, bcs::from_bytes::<<AsyncProtocol as twopc_mpc::sign::Protocol>::Message>(), )
+        (
+            auxiliary_auxiliary_input,
+            bcs::from_bytes::<<AsyncProtocol as twopc_mpc::sign::Protocol>::Message>(),
+            bcs::from_bytes::<
+                <AsyncProtocol as twopc_mpc::sign::Protocol>::DecentralizedPartyDKGOutput,
+            >,
+            bcs::from_bytes::<
+                <AsyncProtocol as twopc_mpc::sign::Protocol>::Presign,
+            >,
+            bcs::from_bytes::<
+                <AsyncProtocol as twopc_mpc::sign::Protocol>::SignMessage,
+            >,
+            bcs::from_bytes::<
+                <AsyncProtocol as twopc_mpc::sign::Protocol>::DecryptionKeySharePublicParameters,
+            >,
+        )
     }
 }
+
+// <(Self::EncryptionOfSecretKeyShareRoundAuxiliaryInput, Self::Message, Self::DecentralizedPartyDKGOutput, Self::Presign, Self::SignMessage, Self::DecryptionKeySharePublicParameters)>
