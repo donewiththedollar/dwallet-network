5bfcf2780087c0bb5fbb82efaf154cca618de9ba
Itay <itaylevy134@gmail.com>
2024-11-11 13:15:28 +0200

    deserialization works

--- crates/class-groups-constants/src/main.rs
+++ crates/class-groups-constants/src/main.rs
@@ -1,5 +1,8 @@
-use class_groups_constants::decryption_key;
+use twopc_mpc::tests::setup_class_groups_secp256k1;
+use class_groups_constants::{decryption_key, protocol_public_parameters};
 
 fn main() {
-    let decyption_key = decryption_key();
+    let public = protocol_public_parameters();
+    let secret = decryption_key();
+    println!("{:?}\n{:?}", public, secret);
 }
