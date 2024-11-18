f96662b40cdeb9b23bb48c91101925faee0e1ddc
Itay <itaylevy134@gmail.com>
2024-11-18 12:04:54 +0200

    keep working

--- crates/pera-swarm-config/src/main.rs
+++ crates/pera-swarm-config/src/main.rs
@@ -1,13 +1,11 @@
 use group::PartyID;
+use twopc_mpc::setup_class_groups_secp256k1;
 use twopc_mpc::sign::deal_blockchain_secret_shares;
 use class_groups_constants::{decryption_key, protocol_public_parameters};
 
 fn main() {
-    let (mut decryption_key_shares, decryption_key_share_public_parameters) =
-        deal_blockchain_secret_shares(
-            3, 4,
-            protocol_public_parameters(), decryption_key()
-        );
-    let clone = decryption_key_share_public_parameters.clone();
-    println!("Decryption key shares: {:?}", clone);
+    let (protocol_public_parameters, decryption_key) = setup_class_groups_secp256k1();
+    println!("Decryption key: {:?}", &base64::encode(bcs::to_bytes(&decryption_key.decryption_key).unwrap()));
+    println!("Encryption key: {:?}", &base64::encode(bcs::to_bytes(&decryption_key.encryption_key).unwrap()));
+    println!("Public parameters: {:?}", &base64::encode(bcs::to_bytes(&protocol_public_parameters).unwrap()));
 }
