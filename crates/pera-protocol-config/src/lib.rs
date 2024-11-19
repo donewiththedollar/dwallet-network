28aed050eb19b5dae18a03d378af7ad2c839a26f
Itay <itaylevy134@gmail.com>
2024-11-19 13:52:47 +0200

    chain run successfully

--- crates/pera-protocol-config/src/lib.rs
+++ crates/pera-protocol-config/src/lib.rs
@@ -2292,7 +2292,7 @@ impl ProtocolConfig {
                     // Only enable random beacon on devnet
                     if chain != Chain::Mainnet && chain != Chain::Testnet {
                         cfg.feature_flags.random_beacon = true;
-                        cfg.random_beacon_reduction_lower_bound = Some(1600);
+                        cfg.random_beacon_reduction_lower_bound = Some(1);
                         cfg.random_beacon_dkg_timeout_round = Some(3000);
                         cfg.random_beacon_min_round_interval_ms = Some(150);
                     }
@@ -2498,7 +2498,7 @@ impl ProtocolConfig {
                     // Enable random beacon on testnet.
                     if chain != Chain::Mainnet {
                         cfg.feature_flags.random_beacon = true;
-                        cfg.random_beacon_reduction_lower_bound = Some(1600);
+                        cfg.random_beacon_reduction_lower_bound = Some(1);
                         cfg.random_beacon_dkg_timeout_round = Some(3000);
                         cfg.random_beacon_min_round_interval_ms = Some(200);
                     }
@@ -2658,7 +2658,7 @@ impl ProtocolConfig {
                 54 => {
                     // Enable random beacon on mainnet.
                     cfg.feature_flags.random_beacon = true;
-                    cfg.random_beacon_reduction_lower_bound = Some(1000);
+                    cfg.random_beacon_reduction_lower_bound = Some(1);
                     cfg.random_beacon_dkg_timeout_round = Some(3000);
                     cfg.random_beacon_min_round_interval_ms = Some(500);
 
